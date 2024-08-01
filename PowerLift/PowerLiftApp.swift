50c160ccf29baaa21080daf0b436216bf82c496d
Kevin <craykev2@gmail.com>
2024-07-31 23:26:36 -0400

    Initial Commit

---
+++ PowerLift/PowerLiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerLiftApp.swift
+//  PowerLift
+//
+//  Created by Kevin Atapattu on 2024-07-31.
+//
+
+import SwiftUI
+
+@main
+struct PowerLiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerLiftTests/PowerLiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  PowerLiftTests.swift
+//  PowerLiftTests
+//
+//  Created by Kevin Atapattu on 2024-07-31.
+//
+
+import Testing
+
+struct PowerLiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
